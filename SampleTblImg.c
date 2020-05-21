758448675f7ac4357d1935204c9b14b8af53226c
Jacob Hageman <jacob.hageman@nasa.gov>
2020-05-21 11:18:08 -0400

    Fix #32, Apply standard code style

--- SampleTblImg.c
+++ SampleTblImg.c
@@ -20,7 +20,7 @@
 **
 */
 
-#include "cfe_tbl_filedef.h"  /* Required to obtain the CFE_TBL_FILEDEF macro definition */
+#include "cfe_tbl_filedef.h" /* Required to obtain the CFE_TBL_FILEDEF macro definition */
 
 /*
 ** The following is an example of a data structure the application may have declared
@@ -28,17 +28,17 @@
 */
 typedef struct
 {
-    int     Int1;
-    int     Int2;
-    int     Int3;
-    char    Char1;
+    int  Int1;
+    int  Int2;
+    int  Int3;
+    char Char1;
 } MyTblStruct_t;
 
 /*
 ** The following is an example of the declaration statement that defines the desired
 ** contents of the table image.
 */
-MyTblStruct_t MyTblStruct = { 0x01020304, 0x05060708, 0x090A0B0C, 0x0D };
+MyTblStruct_t MyTblStruct = {0x01020304, 0x05060708, 0x090A0B0C, 0x0D};
 
 /*
 ** The macro below identifies:
@@ -47,4 +47,4 @@ MyTblStruct_t MyTblStruct = { 0x01020304, 0x05060708, 0x090A0B0C, 0x0D };
 **    3) a brief description of the contents of the file image
 **    4) the desired name of the table image binary file that is cFE compatible
 */
-CFE_TBL_FILEDEF(MyTblStruct, MyApp.TableName, Table Utility Test Table, MyTblDefault.bin )
+CFE_TBL_FILEDEF(MyTblStruct, MyApp.TableName, Table Utility Test Table, MyTblDefault.bin)
