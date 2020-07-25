96dca594e317257f9d0f509ed24d92d9a8ec6e4b
Gerardo E. Cruz-Ortiz <gerardo.e.cruz-ortiz@nasa.gov>
2020-07-25 17:55:56 -0400

    HOTFIX IC-20200624, Remove commented code

--- elf2cfetbl_version.h
+++ elf2cfetbl_version.h
@@ -18,65 +18,49 @@
 **  limitations under the License.
 */
 
-/*
- *  File: elf2cfetbl_version.h
- *
- *  Purpose:
- *  Provide version identifiers for the ELF to cFE Table Converter.
- *  See cfe documentation for version and build number and description
- */
 
+/*! @file elf2cfetbl_version.h
+ * @brief Purpose:
+ *  @details Provide version identifiers for the ELF to cFE Table Converter. @n
+ *  See @ref cfsversions for version and build number and description
+ * 
+ */
 #ifndef ELF2CFETBL_VERSION_H
 #define ELF2CFETBL_VERSION_H
 
-
-/* Development Build Macro Definitions */
-/** @brief Number of commits since baseline v3.1.0 */
-#define ELF2CFETBL_BUILD_NUMBER 36 
-#define ELF2CFETBL_BUILD_BASELINE "v3.1.0+dev"
+/*
+ * Development Build Macro Definitions 
+ */
+#define ELF2CFETBL_BUILD_NUMBER 36 /*!< @brief Number of commits since baseline */
+#define ELF2CFETBL_BUILD_BASELINE "v3.1.0+dev" /*!< @brief Development Build: git tag that is the base for the current */
 
 /*
  * Version Macro Definitions
- * These are only used for OFFICIAL release builds.
  */
-#define ELF2CFETBL_MAJOR_VERSION 3
-#define ELF2CFETBL_MINOR_VERSION 1
-#define ELF2CFETBL_REVISION      0
-#define ELF2CFETBL_MISSION_REV   0
+#define ELF2CFETBL_MAJOR_VERSION 3 /*!< @brief ONLY APPLY for OFFICIAL releases. Major version number. */
+#define ELF2CFETBL_MINOR_VERSION 1 /*!< @brief ONLY APPLY for OFFICIAL releases. Minor version number. */
+#define ELF2CFETBL_REVISION      0 /*!< @brief ONLY APPLY for OFFICIAL releases. Revision version number. */
+#define ELF2CFETBL_MISSION_REV   0 /*!< @brief ONLY USED by MISSION Implementations. Mission revision */
 
 /*
  * Tools to construct version string
- */
-#define ELF2CFETBL_STR_HELPER(x) #x
-#define ELF2CFETBL_STR(x)        ELF2CFETBL_STR_HELPER(x)
+ */ 
+#define ELF2CFETBL_STR_HELPER(x) #x /*!< @brief Helper function to concatenate strings from integer macros */
+#define ELF2CFETBL_STR(x)        ELF2CFETBL_STR_HELPER(x) /*!< @brief Helper function to concatenate strings from integer macros */
 
-/* Development Build Format for ELF2CFETBL_VERSION */
-/* Baseling git tag + Number of commits since baseline, see elf2cfetbl_buildnumber.h */ 
+/*! @brief Development Build Version Number. 
+ * @details Baseline git tag + Number of commits since baseline. @n
+ * See @ref cfsversions for format differences between development and release versions.
+ */
 #define ELF2CFETBL_VERSION ELF2CFETBL_BUILD_BASELINE ELF2CFETBL_STR(ELF2CFETBL_BUILD_NUMBER)
 
-/* Development Build Format for ELF2CFETBL_VERSION_STRING */
+/*! @brief Development Build Version String.
+ * @details Reports the current development build's baseline, number, and name. Also includes a note about the latest official version. @n
+ * See @ref cfsversions for format differences between development and release versions. 
+*/     
 #define ELF2CFETBL_VERSION_STRING                                                         \
     " elf2cfetbl Development Build\n"                                                     \
     " " ELF2CFETBL_VERSION " (Codename: Bootes)\n" /* Codename for current development */ \
     " Last Offical Release: elf2cfetbl v3.1.0"     /* For full support please use official release version */
-
-/* Use the following templates for Official Releases ONLY */
-  /* Official Release format for ELF2CFETBL_VERSION */
-  /* 
-    #define ELF2CFETBL_VERSION "v"               \    
-    ELF2CFETBL_STR(ELF2CFETBL_MAJOR_VERSION) "." \
-    ELF2CFETBL_STR(ELF2CFETBL_MINOR_VERSION) "." \
-    ELF2CFETBL_STR(ELF2CFETBL_REVISION) "."      \
-    ELF2CFETBL_STR(ELF2CFETBL_MISSION_REV)         
-  */
-  
-  
-  /* Official Release format for ELF2CFETBL_VERSION_STRING */
-  /*
-    #define ELF2CFETBL_VERSION_STRING "elf2cfetbl " ELF2CFETBL_VERSION
-  */
-  
-/* END TEMPLATES */
-
 
 #endif /* ELF2CFETBL_VERSION_H */
